f272bc0c01155e3d7ee2e621e3578c71a14e9520
Jorge Bierhals Ramos <jorge.bierhals@engdb.com.br>
2023-08-08 16:01:49 -0300

    chore: add commitlint and husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Hello</h2>
+      <h2>
+        Template with Next.js, TailwindCSS, Typescript, ESLint, Prettier, Husky,
+        Lint Staged, Commitlint, Jest, React Testing Library and Github Actions.
+      </h2>
     </main>
   )
 }
